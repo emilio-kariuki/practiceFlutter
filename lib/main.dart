b07ffd6f9b7def0f81c7b9f8e693d21ff5e3a215
emilio kariuki <70817006+emiliokariuki@users.noreply.github.com>
2022-01-10 08:53:14 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,90 +9,73 @@ void main() {
 }
 
 class home extends StatelessWidget {
-  // const name({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text(
-            "Home",
-            style: TextStyle(
-              fontFamily: "IndieFlower",
-              fontWeight: FontWeight.bold,
-              color: Colors.grey[800],
-              letterSpacing: 0.9,
-              fontSize: 40.0,
-            ),
-          ),
-        ),
-      ),
-      // body: Center(
-      //   child: Text(
-      //     "This is the main Page",
-      //     style: TextStyle(
-      //       fontFamily: "Island",
-      //       fontSize: 30.0,
-      //       fontWeight: FontWeight.bold,
-      //       letterSpacing: 0.6,
-      //     ),
-      //   ),
-      // ),
-      // body: Container(
-      //     padding: EdgeInsets.all(40.0),
-      //     margin: EdgeInsets.fromLTRB(30.0,40.0, 30.0, 20.0),
-      //     color: Colors.grey[800],
-      //     child: Image.network(
-      //         "https://c4.wallpaperflare.com/wallpaper/314/285/254/technology-computer-blue-wallpaper-preview.jpg")),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Text("Hello wrord"),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.thumb_up,
-              size: 30.0,
-              color: Colors.amber,
-            ),
-          ),
-          Container(
-            color: Colors.cyan,
-            padding: EdgeInsets.all(30.0),
+        appBar: AppBar(
+          title: Center(
             child: Text(
-              "Container",
+              "Home",
               style: TextStyle(
                 fontFamily: "IndieFlower",
-                fontSize: 30.0,
                 fontWeight: FontWeight.bold,
-                letterSpacing: 0.4,
-                color: Colors.grey[700],
+                fontSize: 30.0,
+                color: Colors.grey,
+                letterSpacing: 0.3,
               ),
             ),
           ),
-        ],
-      ),
-      //  body: Center(
-      //   child: IconButton(
-      //     onPressed: () {
-      //       print("you clicked me");
-      //     },
-      //     icon: Icon(
-      //       Icons.add_a_photo,
-      //       size: 40.0,
-      //       color: Colors.amber,
-      //     ),
-      //   ),
-      // ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          print("Hello world");
-        },
-        label: Text("Click"),
-        icon: Icon(Icons.thumb_up),
-        backgroundColor: Colors.red[400],
-      ),
-    );
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              Text("Hello world"),
+              Text("Hell this is Kenya"),
+            ],
+          ),
+          // child: Row(
+          //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //   crossAxisAlignment: CrossAxisAlignment.end,
+          //   children: [
+          //     Text(
+          //       "Body page",
+          //       style: TextStyle(
+          //         fontFamily: "Island",
+          //         fontWeight: FontWeight.bold,
+          //         letterSpacing: 1.0,
+          //         fontSize: 30.0,
+          //         color: Colors.grey[800],
+          //       ),
+          //     ),
+          //     RaisedButton.icon(
+          //       onPressed: () {},
+          //       icon: Icon(Icons.upload,
+          //       size: 20.0,
+          //       color: Colors.amber),
+          //       label: Text("Upload"),),
+          //   ],
+          // ),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            print("hello world");
+          },
+          label: Text(
+            "Click me",
+            style: TextStyle(
+              fontFamily: "IndieFlower",
+              fontWeight: FontWeight.bold,
+              fontSize: 30.0,
+              color: Colors.grey,
+              letterSpacing: 0.3,
+            ),
+          ),
+          icon: Icon(
+            Icons.thumb_up,
+            size: 20.0,
+            color: Colors.amber,
+          ),
+          backgroundColor: Colors.red[800],
+        ));
   }
 }
